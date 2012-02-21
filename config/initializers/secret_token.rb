540bf16cb56ce16ed18e3ef6bc030e22bf4ca4d6
Mike Schwab <mike.schwab@gmail.com>
2012-02-20 22:27:55 -0500

    sign up and log in are possible

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stickers::Application.config.secret_token = '1981caf325ff84206423152afd7e87637fd7081637f7ea00c0db3892fdc46ed06fa034f7d4c3aca0a130d4a06bb6776b5c532ef12dc1a0a8ff72d89f4bebcadd'
