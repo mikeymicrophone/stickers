49468a6e78375af2be3ba9b74ee0096fe79b4b97
Mike Schwab <mike.schwab@gmail.com>
2012-03-01 02:33:44 -0500

    compile fancybox into manifest assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//
 //= require jquery
 //= require jquery_ujs
+//= require jquery.fancybox
+//= require jquery.fancybox-buttons
+//= require jquery.fancybox-thumbs
+//= require jquery.mousewheel-3.0.6.pack
 //= require_tree .
