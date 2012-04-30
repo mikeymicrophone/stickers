2fae342f2526737b524307bb94eb278ec63d957c
Mike Schwab <mike.schwab@gmail.com>
2012-04-30 10:57:15 -0400

    new font and little styling

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :invitee_id
