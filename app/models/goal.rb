04c0e5227b46117649def2fe932c00d58497a08c
Mike Schwab <mike.schwab@gmail.com>
2012-05-11 04:04:18 -0400

    new endeavor link scopes available goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,4 +3,5 @@ class Goal < ActiveRecord::Base
   has_many :members, :through => :endeavors
   
   scope :alphabetical, :order => 'title'
+  scope :not_endeavored_upon_by, lambda { |member| where('id not in (?)', member.endeavors.map(&:goal_id)) }
 end
