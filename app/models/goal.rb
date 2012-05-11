8cbb7755e478c002eb9615f35f846539f34441c0
Mike Schwab <mike.schwab@gmail.com>
2012-05-11 17:54:58 -0400

    details on goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,10 @@
 class Goal < ActiveRecord::Base
   has_many :endeavors
   has_many :members, :through => :endeavors
+  has_many :details, :as => :target
   
   scope :alphabetical, :order => 'title'
   scope :not_endeavored_upon_by, lambda { |member| where('id not in (?)', member.endeavors.map(&:goal_id)) }
+  
+  alias_attribute :name, :title
 end
